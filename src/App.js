81929ec8fe23d4034ef959b96374610fabcb33ee
Crqzytaco <shitoriapereira28@gmail.com>
2026-02-25 15:41:28 -0500

    ///updated for teacher page if i accidentally changed the student page tell me so i can unfuck it up s2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StudentPage from "./pages/StudentPage";
 import TeacherPage from "./pages/TeacherPage";
 import LoginPage from "./pages/LoginPage";
-
+import CourseManagementPage from "./pages/CourseManagementPage";
+import { courses as hardcodedCourses } from "./components/Data";
 function App(){
+    const [courses, setCourses] = useState(hardcodedCourses);
+
+    const updateCourse = (updatedCourse) => {
+        setCourses(prevCourses => {
+          const existingCourse = prevCourses.find(course => course.id === updatedCourse.id);
+           if (existingCourse) {
+            // Update existing course
+            return prevCourses.map(course =>
+                course.id === updatedCourse.id ? updatedCourse : course
+            );
+        } else {
+            // Add new course
+            return [...prevCourses, updatedCourse];
+        }
+        });    
+    };
+
+  
+
   return(
+    
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage/>}/>
         <Route path="/student/*" element={<StudentPage/>}/>
-        <Route path="/teacher" element={<TeacherPage/>}/>
+        <Route path="/teacher" element={<TeacherPage courses={courses} onUpdateCourse={updateCourse}/>}/>
+        <Route path="/course/:id" element={<CourseManagementPage courses={courses} onUpdateCourse={updateCourse} />} />
       </Routes>
     </Router>
   );
