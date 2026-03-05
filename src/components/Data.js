e355259f68bd68695ed4d2a85d21501a5ecb650d
TasneemChowdhury <chowdhurytasneem102@gmail.com>
2026-03-04 21:58:54 -0500

    Added hard-coded courses to teacher page
    Added two hard-coded courses to teacher page; Engr 233 and comp 249.

--- src/components/Data.js
+++ src/components/Data.js
@@ -13,4 +13,32 @@ export const courses = [ //just the hardcoded course
             {type: 'Final', weight: 40, completed: 99},
         ],
     },
+    {
+        id: 2,
+        code: 'ENGR 233',
+        name: 'Applied advanced calculus',
+        term: 'Winter 2026',
+        isActive: true,
+        totalStudents: 180,
+        assessments: [
+            {type: 'Quiz', weight: 10, completed: 120 },
+            {type: 'Assignment', weight: 10, completed: 38},
+            {type: 'Midterm', weight: 20, completed: 168},
+            {type: 'Final', weight: 60, completed: 178},
+        ],
+    },
+    {
+        id: 3,
+        code: 'COMP 249',
+        name: 'Object-oriented programming II',
+        term: 'Winter 2026',
+        isActive: true,
+        totalStudents: 127,
+        assessments: [
+            {type: 'Quiz', weight: 10, completed: 120},
+            {type: 'Assignment', weight: 20, completed: 50},
+            {type: 'Midterm', weight: 20, completed: 127},
+            {type: 'Final', weight: 50, completed: 0},
+        ],
+    },
 ];
