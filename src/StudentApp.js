21707fb02255a8c6f81821f90eaf71e9d35e9f03
Mesa237 <madjofoariane@gmail.com>
2026-03-05 13:38:35 -0500

    Add files via upload

---
+++ src/StudentApp.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StudentProfile from "./pages/StudentProfile";
+import TeacherPage from "./pages/TeacherPage";
+import LoginPage from "./pages/LoginPage";
+import CourseManagementPage from "./pages/CourseManagementPage";
+import { courses as hardcodedCourses } from "./components/Data";
+function App(){
+    const [courses, setCourses] = useState(hardcodedCourses); //initiates the array with the data stored in Data.js needs to be here so updates work
+
+    const updateCourse = (updatedCourse) => { //function to update courses
+        setCourses(prevCourses => {
+          const existingCourse = prevCourses.find(course => course.id === updatedCourse.id);
+           if (existingCourse) {
+            // Update existing course
+            return prevCourses.map(course =>
+                course.id === updatedCourse.id ? updatedCourse : course
+            );
+        } else {
+            // Add new course
+            return [...prevCourses, updatedCourse];
+        }
+        });    
+    };
+
+  
+{/*needs to have both courses and onUpdateCourses to work since it has hardcoded and not valuers*/}
+    
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/student/*" element={<StudentProfile/>}/>
+        <Route path="/teacher" element={<TeacherPage courses={courses} onUpdateCourse={updateCourse}/>}/>
+        <Route path="/course/:id" element={<CourseManagementPage courses={courses} onUpdateCourse={updateCourse} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
