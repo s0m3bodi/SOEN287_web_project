957a13006b71c964e36650f187a0d1ab8bb21c47
fgul <158237833+s0m3bodi@users.noreply.github.com>
2026-03-06 13:39:02 -0500

    deleting redundancies

--- src/components/TeacherSidebar.js
+++ src/components/TeacherSidebar.js
@@ -10,21 +10,16 @@ const TeacherSidebar = ({courses, onAddCourse}) => {//courses are the hardcoded 
 
     return(
         <div className="sidebar">
-            <h2>Courses</h2> {/*just what appears on top of the sidebar*/}
-
-                <Link to="/teacher"> {/*home button that links to the home page*/}
-                    <button>Home</button>
-                </Link>
-
+            <h2>Courses</h2>
                 {/*goes through array of courses(hardcoded) and does whats inside the ()*/} 
                 {courses.map(course =>(
-                    <Link key={course.id} to={`/course/${course.id}`}> {/*creates a link for each array id to=is how its gonna appear on the search bar on the top key so they have different ids*/}
-                        <div>{course.name}</div> {/*whats written on the link*/}
+                    <Link key={course.id} to={`/course/${course.id}`}> {/*creates a link for each array id to= is how its gonna appear on the search bar on the top key so they have different ids*/}
+                        <div>{course.name}</div> 
                     </Link>
                 ))}
 
             <div>
-                <button onClick={onAddCourse}>Add Course</button> {/*button to add a course needs to stay a button*/}
+                <button onClick={onAddCourse}>Add Course</button> {/*button to add a course*/}
             </div>
             
             <div className="signOut">
