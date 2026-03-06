c9ece13222e7e75b2c28eabe61e5f0df6b67eb1e
fgul <158237833+s0m3bodi@users.noreply.github.com>
2026-03-06 12:36:57 -0500

    teacher sign out

--- src/components/TeacherSidebar.js
+++ src/components/TeacherSidebar.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate, userNavigate } from "react-router-dom";
 import '../pagesCSS/TeacherSidebar.css'
-//supposed to be the same look as student sidebar to be pretty can change it if you want
-const TeacherSidebar = ({ courses, onAddCourse }) => { //courses are the hardcoded ones //onAddCourse is for the new ones
-    return (
+import { courses } from "./Data";
+
+const TeacherSidebar = ({courses, onAddCourse}) => {//courses are the hardcoded ones, onAddCourse is for the new ones
+    const navigate = useNavigate();
+    const handleLogout = () =>{
+        navigate('/');
+    };
+
+    return(
         <div className="sidebar">
-        <h2>Courses</h2> {/*just what apeears on top of sidebar */}
-        <Link to="/teacher"> {/* this is the home button that links to the home page can change to not be a button but dont change link */}
-         <button>Home</button> {/*whats written on the button*/}
-        </Link>
-        {/*courses.map goes though the array courses(hardcoded) and does whats inside the()*/}
-        {courses.map(course => ( 
-            <Link key={course.id} to={`/course/${course.id}`}>{/*creates a link for each array id to=is how its gonna appear on the search bar on the top key is just so they have different ids*/}
-                <div>{course.name}</div> {/*whats written on the link*/}
-            </Link>
-        ))}
-        <button onClick={onAddCourse}>Add Course</button> {/*button to add a course needs to stay a button*/}
+            <h2>Courses</h2> {/*just what appears on top of the sidebar*/}
+
+                <Link to="/teacher"> {/*home button that links to the home page*/}
+                    <button>Home</button>
+                </Link>
+
+                {/*goes through array of courses(hardcoded) and does whats inside the ()*/} 
+                {courses.map(course =>(
+                    <Link key={course.id} to={`/course/${course.id}`}> {/*creates a link for each array id to=is how its gonna appear on the search bar on the top key so they have different ids*/}
+                        <div>{course.name}</div> {/*whats written on the link*/}
+                    </Link>
+                ))}
+
+            <div>
+                <button onClick={onAddCourse}>Add Course</button> {/*button to add a course needs to stay a button*/}
+            </div>
+            
+            <div className="signOut">
+                <button onClick={handleLogout}>
+                    Sign Out
+                </button>
+            </div>
         </div>
     );
-};
+}
 
 
 
--- src/pages/TeacherPage.js
+++ src/pages/TeacherPage.js
@@ -13,60 +13,56 @@ const TeacherPage = ({ courses, onUpdateCourse }) => { //home page with hardcode
     const navigate = useNavigate(); 
     
 
-const handleInputChange = (e) => { //handles the input for new courses (like set in java)
-    setNewCourse({ ...newCourse, [e.target.name]: e.target.value }); 
-};
+    const handleInputChange = (e) => { //handles the input for new courses (like set in java)
+        setNewCourse({ ...newCourse, [e.target.name]: e.target.value }); 
+    };
 
-const addCourse = () => { //adds new course to array
-    const newId = courses.length > 0 ? Math.max(courses.map(course => course.id)) + 1 : 1; //resizes array to fit 1 more 
-    const courseToAdd = { id: newId, ...newCourse, isActive: true, totalStudents: 300 }; //creates new array object then copies new course in it, sets active to true and total students to 300 by default
+    const addCourse = () => { //adds new course to array
+        const newId = courses.length > 0 ? Math.max(courses.map(course => course.id)) + 1 : 1; //resizes array to fit 1 more 
+        const courseToAdd = { id: newId, ...newCourse, isActive: true, totalStudents: 300 }; //creates new array object then copies new course in it, sets active to true and total students to 300 by default
     
-    onUpdateCourse(courseToAdd); //updates everything
+        onUpdateCourse(courseToAdd); //updates everything
 
-    setNewCourse({ code: '', name: '', term: '', assessments: [] }); //resets newcourse if want to add more
-    setShowAddModal(false); //closes the menu for adding courses
-};
+        setNewCourse({ code: '', name: '', term: '', assessments: [] }); //resets newcourse if want to add more
+        setShowAddModal(false); //closes the menu for adding courses
+    };
 
- const toggleActive = (courseId) => { //to make it active/inactive goes by array index
-   onUpdateCourse({ //so it updates
+    const toggleActive = (courseId) => { //to make it active/inactive goes by array index
+        onUpdateCourse({ //so it updates
             ...courses.find(course => course.id === courseId), //searches array for index then copies the info on the updated
             isActive: !courses.find(course => course.id === courseId).isActive  //makes it active/inactive if found
         });
    
-};
+    };
 
 
-return (
-    <div style={{ display: "flex", minHeight: "100vh" }}> {/*anything on style can be moved to future css file*/}
-        <TeacherSidebar courses={courses} onAddCourse={() => setShowAddModal(true)} /> {/*courses={course} prints the array and onAddCourse={( ... makes the menu for adding a course show by setting it active*/}
-      <main style={{ padding: "10px", flex: 1 }}> {/* the filter goes though the array to only print the active ones on the main page*/}
-       {courses.filter(course => course.isActive).map(course => ( 
+    return (
+        <div style={{ display: "flex", minHeight: "100vh" }}> {/*anything on style can be moved to future css file*/}
+            <TeacherSidebar courses={courses} onAddCourse={() => setShowAddModal(true)} /> {/*courses={course} prints the array and onAddCourse={( ... makes the menu for adding a course show by setting it active*/}
+            <main style={{ padding: "10px", flex: 1 }}> {/* the filter goes though the array to only print the active ones on the main page*/}
+            
+                {courses.filter(course => course.isActive).map(course => ( 
                     <div key={course.id} className="course"> 
                         <h2>{course.name}</h2> {/*prints the name can be changed to also have code and session*/}
-                        
+                            
                         <div className='coursecontent'>
                             <h3>Assessments</h3> {/*print assessments with the completion percent*/}
-                        {/*checks if there is any assessments creates a list of them if yes*/}
+                            {/*checks if there is any assessments creates a list of them if yes*/}
                             {course.assessments.length > 0 ? (  
                                 <ul> 
-                                    {/* checks if the assessment is completed and there are students enrolled then computes percentage formated 2 decimal places*/}
+                                {/* checks if the assessment is completed and there are students enrolled then computes percentage formated 2 decimal places*/}
                                     {course.assessments.map((assessment, index) => {
-                                    const completionPercentage = assessment.completed && course.totalStudents > 0 
-                                            ? ((assessment.completed / course.totalStudents) * 100).toFixed(2)
-                                            : 0;
-                                            {/*return the list of assignment with the type and percentage*/}
+                                        const completionPercentage = assessment.completed && course.totalStudents > 0 
+                                                    ? ((assessment.completed / course.totalStudents) * 100).toFixed(2): 0;
+                                                    {/*return the list of assignment with the type and percentage*/}
                                         return ( 
                                             <li key={index}>
                                                 {assessment.type}: {completionPercentage}% of students completed ({assessment.completed}/{course.totalStudents} students)
                                             </li>
-                                        );
-                                    })} 
-                                
+                                            );                
+                                    })}        
                                 </ul>
-                            ) : (
-                                <p>No assessments available.</p> 
-                            )} {/* if no assignments*/}
-
+                            ) : (<p>No assessments available.</p>)} {/* if no assignments*/}
 
                             <button onClick={() => toggleActive(course.id)}> {/* button for actice status*/}
                                 Mark as {course.isActive ? "Inactive" : "Active"}
@@ -74,23 +70,26 @@ return (
                         </div>
                     </div>
                 ))}
-                 {/*menu for new course add */}
+
+                {/*menu for new course add */}
                 {showAddModal && (
                     <div className="addMenu">
                         <h2>Add New Course</h2>
                         <div className="new-courseinfo">
                             <label>Enter course code:
-                            <input className="boxforcode" name="code" value={newCourse.code} onChange={handleInputChange} placeholder="Course Code e.g. SOEN 287" />
+                                <input className="boxforcode" name="code" value={newCourse.code} onChange={handleInputChange} placeholder="Course Code e.g. SOEN 287" />
                             </label>
+
                             <label>Enter course name:
-                            <input  className="boxforname" name="name" value={newCourse.name} onChange={handleInputChange} placeholder="Course Name" />
+                                <input  className="boxforname" name="name" value={newCourse.name} onChange={handleInputChange} placeholder="Course Name" />
                             </label>
+
                             <label>Enter course term:
-                            <input className="boxforterm" name="term" value={newCourse.term} onChange={handleInputChange} placeholder="Term" />
+                                <input className="boxforterm" name="term" value={newCourse.term} onChange={handleInputChange} placeholder="Term" />
                             </label>
                         </div>
-                        <button onClick={addCourse}>Add Course</button>  {/*button at the end to confirm*/}
-                        <button onClick={() => setShowAddModal(false)}>Cancel</button>  {/*in case give up on adding*/}
+                            <button onClick={addCourse}>Add Course</button>  {/*button at the end to confirm*/}
+                            <button onClick={() => setShowAddModal(false)}>Cancel</button>  {/*in case give up on adding*/}
                     </div>
                 )}
             </main>
