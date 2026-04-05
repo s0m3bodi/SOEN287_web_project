e1f999e3cc07f39fb3060b2426f8a1a3d19b2b44
Crqzytaco <shitoriapereira28@gmail.com>
2026-04-05 17:02:58 -0400

    Delete src/pages/Profile.js

--- src/pages/Profile.js
+++
@@ -1,9 +0,0 @@
-class profile{
-    constructor(firstname,lastName, studentID, emailAddress, courses ){
-        this.courses = courses;
-        this.emailAddress = emailAddress;
-        this.firstname = firstname;
-        this.studentID = studentID;
-        this.lastName = lastName;
-    }
-}
