58877b0c752d9847b8af9e13c2c46e3ddd197370
Crqzytaco <shitoriapereira28@gmail.com>
2026-04-05 17:01:49 -0400

    Delete src/pages/Assessment.js

--- src/pages/Assessment.js
+++
@@ -1,9 +0,0 @@
-class Assessment {
-    constructor(type = "", name = "", deadline = "", weight = 0){
-        this.type = type;
-        this.name = name;
-        this.deadline = deadline;
-        this.weight = weight;
-    }
-}
-export default Assessment;
